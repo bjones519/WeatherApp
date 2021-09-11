c08ff71016fae200bddfa9422328a7889759235e
Brittney jones <brittneyjones@Brittneys-MacBook-Pro.local>
2021-09-11 08:48:24 -0400

    adding comments to application.py

--- application.py
+++ application.py
@@ -2,18 +2,22 @@ from flask import Flask, request, Response, render_template
 import requests
 
 application = app = Flask(__name__)
+#enables debug mode
 app.config['DEBUG']= True
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
+
+#route to return the weather for "Valley Stream"
 @app.route('/')
 def main():
+    #api endpoint with the query blank for the city
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=7e9f0ff1d2f7ec3616157c1995baeb21'
     city = 'Valley Stream'
 
+    #inputs the city into the query in the url and gives the json output
     response= requests.get(url.format(city)).json()
     
+    #dictionary to hold the city, temp, description and icon
     weather = {
         'city' : city,
         'temperature' : response['main']['temp'],
@@ -23,4 +27,4 @@ def main():
 
     print(weather)   
     
-    return render_template('weather.html', weather= weather)
+    return render_template('weather.html', weather = weather)
